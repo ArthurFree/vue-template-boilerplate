f7263be74c420a8bdd5ecddcaa26006d66d97bcb
Mr_LC <lc940401@gmail.com>
2017-05-01 13:41:42 +0800

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,10 @@ module.exports = {
         // import/no-extraneous-dependencies
         "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
         // no-plusplus 一元操作符会自动添加分号，不同的空白会改变源代码的含义
-        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }]
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+        // no-console
+        "no-console": process.env.NODE_ENV === 'production' ? 2 : 0,
+        // no-debugger, allow debugger during development
+        "no-debugger": process.env.NODE_ENV === 'prroduction' ? 2 : 0
     }
 };
--- build/build.js
+++ build/build.js
@@ -4,42 +4,42 @@ process.env.NODE_ENV = 'production'
 
 // Elegant terminal spinner
 // https://www.npmjs.com/package/ora
-var ora = require('ora')
+const ora = require('ora');
 
 // The UNIX command rm -rf for node.
 // https://www.npmjs.com/package/rimraf
-var rm = require('rimraf')
+const rm = require('rimraf');
 
-var path = require('path')
+const path = require('path');
 
 // 见 build/check-versions.js
-var chalk = require('chalk')
+const chalk = require('chalk');
 
-var webpack = require('webpack')
+const webpack = require('webpack');
 
-var config = require('../config')
+const config = require('../config');
 
-var webpackConfig = require('./webpack.prod.conf')
+const webpackConfig = require('./webpack.prod.conf');
 
-var spinner = ora('building for prroduction...')
-spinner.start()
+const spinner = ora('building for prroduction...');
+spinner.start();
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     if (err) throw err;
     webpack(webpackConfig, function (err, stats) {
         spinner.stop()
-        if (err) throw err
+        if (err) throw err;
         process.stdout.write(stats.toString({
             colors: true,
             modules: false,
             children: false,
             chunks: false,
-            chunkModules: false
+            chunkModules: false,
         }) + '\n\n');
         console.log(chalk.cyan('   Build complete.\n'));
         console.log(chalk.yellow(
             '   Tip: built files are meant to be served over an HTTP server.\n' + 
             '   Opeaning index.html over file:// worn\'t work.\n'
-        ))
-    })
-})
+        ));
+    });
+});
