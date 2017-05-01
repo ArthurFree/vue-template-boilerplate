bc5e9ebd7e21544a0aab11f82811b26592b03a63
Mr_LC <lc940401@gmail.com>
2017-05-01 14:31:26 +0800

    add demo/test_flow

---
+++ demo/test_flow.js
@@ -0,0 +1,12 @@
+// function test(name: string):void {
+//     console.log('hello, ', name);
+// }
+
+// export default test;
+
+
+function test(name) {
+    console.log(`Hello, ${name}`);
+}
+
+export default test;
